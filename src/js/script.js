2ae81642701f30f011548e7a8b7b71bd755c16d7
kastastin <kastastin.main@gmail.com>
2025-01-21 23:11:34 +0200

    initial commit (1920px)

---
+++ src/js/script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// <-- What Harper Hotspots -->
+document.addEventListener("DOMContentLoaded", () => {
+  const hotspots = document.querySelectorAll(".what-harper__hotspot");
+
+  let activeHotspot = hotspots[0];
+  activeHotspot.classList.add("active");
+
+  hotspots.forEach((hotspot) => {
+    hotspot.addEventListener("click", () => {
+      if (activeHotspot !== hotspot) {
+        activeHotspot.classList.remove("active");
+      }
+
+      hotspot.classList.toggle("active");
+      activeHotspot = hotspot.classList.contains("active") ? hotspot : hotspots[0];
+    });
+  });
+
+  // Close on outside click
+  document.addEventListener("click", (e) => {
+    if (!e.target.closest(".what-harper__hotspot")) {
+      hotspots.forEach((hotspot) => hotspot.classList.remove("active"));
+      hotspots[0].classList.add("active"); // Keep first open
+      activeHotspot = hotspots[0];
+    }
+  });
+});
+
+// <-- Swiper -->
+const swiper = new Swiper(".feedback__swiper", {
+  loop: false,
+  spaceBetween: 30,
+  effect: "cards",
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+    formatFractionCurrent: function (number) {
+      return ("0" + number).slice(-2);
+    },
+    formatFractionTotal: function (number) {
+      return ("0" + number).slice(-2);
+    },
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
